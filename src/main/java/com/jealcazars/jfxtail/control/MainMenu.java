2926a85e92131ebe06b31c0cb88867ea68a5a8b6
Juanen <Juanen@DESKTOP-CHIJFAK>
2018-06-11 23:05:21 +0200

    -

--- src/main/java/com/jealcazars/jfxtail/control/MainMenu.java
+++ src/main/java/com/jealcazars/jfxtail/control/MainMenu.java
@@ -2,7 +2,9 @@ package com.jealcazars.jfxtail.control;
 
 import java.io.File;
 
-import com.jealcazars.jfxtail.control.TabFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.jealcazars.jfxtail.view.FXMLViewLoader;
 
 import javafx.event.ActionEvent;
@@ -12,6 +14,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 
 public class MainMenu extends VBox {
+	private static final Logger LOG = LoggerFactory.getLogger(MainMenu.class);
 
 	public MainMenu() {
 		FXMLViewLoader.load(this, "MainMenu.xml");
@@ -19,6 +22,7 @@ public class MainMenu extends VBox {
 
 	@FXML
 	private void handleOpenAction(final ActionEvent event) {
+		LOG.debug("handleOpenAction");
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open Resource File");
 
@@ -29,5 +33,11 @@ public class MainMenu extends VBox {
 			TabFile tabFile = new TabFile(file, getScene());
 			tabPane.getTabs().add(tabFile);
 		}
+	}
+
+	@FXML
+	private void handleHighlightsAction(final ActionEvent event) {
+		LOG.debug("handleHighlightsAction");
+
 	}
 }

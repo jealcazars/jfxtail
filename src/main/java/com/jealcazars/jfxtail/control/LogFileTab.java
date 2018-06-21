f5491fc0981a1eca374820cbbb46b534f5c00819
Juanen <Juanen@DESKTOP-CHIJFAK>
2018-06-21 08:42:02 +0200

    File can be open only one time Icons shows when new messages are shown

--- src/main/java/com/jealcazars/jfxtail/control/LogFileTab.java
+++ src/main/java/com/jealcazars/jfxtail/control/LogFileTab.java
@@ -22,14 +22,17 @@ import javafx.application.Platform;
 import javafx.geometry.Point2D;
 import javafx.scene.control.Tab;
 import javafx.scene.control.Tooltip;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 
 public class LogFileTab extends Tab implements PropertyChangeListener {
 	private static final Logger LOG = Logger.getLogger(LogFileTab.class.getName());
 
 	File file;
 	FileListener fileListener;
-	private CodeArea codeArea = new CodeArea();
+	CodeArea codeArea = new CodeArea();
 	int linesAlreadyAdded = 0;
+	boolean isSelected;
 
 	private PropertyChangeSupport propertyChangeSupport;
 	VirtualizedScrollPane<CodeArea> virtualizedScrollPane = new VirtualizedScrollPane<>(codeArea);
@@ -43,7 +46,6 @@ public class LogFileTab extends Tab implements PropertyChangeListener {
 		setContent(virtualizedScrollPane);
 		setTooltip(new Tooltip(file.getAbsolutePath()));
 
-		// TODO review if needed
 		codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
 
 		fileListener = new FileListener(file);
@@ -59,6 +61,79 @@ public class LogFileTab extends Tab implements PropertyChangeListener {
 
 	public void addPropertyChangeListener(PropertyChangeListener listener) {
 		propertyChangeSupport.addPropertyChangeListener(listener);
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		if (FileListener.FILE_WAS_MODIFIED.equals(evt.getPropertyName())) {
+			long lastLength = (Long) evt.getOldValue();
+			long newLength = (Long) evt.getNewValue();
+			readFile((int) lastLength, (int) newLength, true);
+		}
+	}
+
+	private synchronized void loadFile() {
+		readFile(0, (int) file.length(), false);
+	}
+
+	private synchronized void readFile(int oldLength, int newLength, boolean append) {
+
+		try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
+			int bytesToRead = newLength - oldLength;
+
+			if (bytesToRead > JfxTailAppPreferences.getBufferSize()) {
+				LOG.fine("bytesToRead bigger than buffer size, some bytes will be ignored");
+				bytesToRead = JfxTailAppPreferences.getBufferSize();
+			}
+
+			byte[] fileContentAsBytes = new byte[bytesToRead];
+			bis.skip(newLength - bytesToRead);
+
+			bis.read(fileContentAsBytes);
+			String[] newLines = new String(fileContentAsBytes, "UTF-8").split("\n");
+
+			Platform.runLater(() -> {
+
+				if (!append) {
+					codeArea.clear();
+					linesAlreadyAdded = 0;
+				}
+
+				boolean added = false;
+
+				for (int i = 0; i < newLines.length; i++) {
+					if (!JfxTailAppPreferences.isFilterActive() || (JfxTailAppPreferences.isFilterActive()
+							&& TextFilterProcessor.lineMustBeAppended(newLines[i]))) {
+						codeArea.appendText(newLines[i]);
+						linesAlreadyAdded++;
+
+						if (linesAlreadyAdded > JfxTailAppPreferences.getMaxLines()) {
+							codeArea.replaceText(0, codeArea.getParagraph(0).length() + 1, "");
+						}
+
+						added = true;
+					}
+				}
+
+				if (added) {
+
+					if (JfxTailAppPreferences.isHighlightActive()) {
+						HighlightFilterProcessor.applyHighlighting(codeArea);
+					}
+
+					if (JfxTailAppPreferences.isFollowTailActive()) {
+						codeArea.scrollBy(new Point2D(0, 10000));
+					}
+
+					if (!isSelected()) {
+						setGraphic(new Rectangle(8, 8, Paint.valueOf("black")));
+					}
+				}
+
+			});
+		} catch (Exception e) {
+			LOG.log(Level.SEVERE, "Error " + e.getMessage(), e);
+		}
 	}
 
 	public void clear() {
@@ -91,67 +166,5 @@ public class LogFileTab extends Tab implements PropertyChangeListener {
 
 	public int getLinesAlreadyAdded() {
 		return linesAlreadyAdded;
-	}
-
-	@Override
-	public void propertyChange(PropertyChangeEvent evt) {
-		if (FileListener.FILE_WAS_MODIFIED.equals(evt.getPropertyName())) {
-			long lastLength = (Long) evt.getOldValue();
-			long newLength = (Long) evt.getNewValue();
-			readFile((int) lastLength, (int) newLength, true);
-		}
-	}
-
-	private synchronized void loadFile() {
-		readFile(0, (int) file.length(), false);
-	}
-
-	private synchronized void readFile(int oldLength, int newLength, boolean append) {
-
-		try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
-			int bytesToRead = newLength - oldLength;
-
-			if (bytesToRead > JfxTailAppPreferences.getBufferSize()) {
-				LOG.fine("bytesToRead bigger than buffer size, some bytes will be ignored");
-				bytesToRead = JfxTailAppPreferences.getBufferSize();
-			}
-
-			byte fileContentAsBytes[] = new byte[bytesToRead];
-			bis.skip(newLength - bytesToRead);
-
-			bis.read(fileContentAsBytes);
-			String[] newLines = new String(fileContentAsBytes, "UTF-8").split("\n");
-
-			Platform.runLater(() -> {
-
-				if (!append) {
-					codeArea.clear();
-					linesAlreadyAdded = 0;
-				}
-
-				for (int i = 0; i < newLines.length; i++) {
-					if (!JfxTailAppPreferences.isFilterActive() || (JfxTailAppPreferences.isFilterActive()
-							&& TextFilterProcessor.lineMustBeAppended(newLines[i]))) {
-						codeArea.appendText(newLines[i]);
-						linesAlreadyAdded++;
-
-						if (linesAlreadyAdded > JfxTailAppPreferences.getMaxLines()) {
-							codeArea.replaceText(0, codeArea.getParagraph(0).length() + 1, "");
-						}
-					}
-				}
-
-				if (JfxTailAppPreferences.isHighlightActive()) {
-					HighlightFilterProcessor.applyHighlighting(codeArea);
-				}
-
-				if (JfxTailAppPreferences.isFollowTailActive()) {
-					codeArea.scrollBy(new Point2D(0, 10000));
-				}
-
-			});
-		} catch (Exception e) {
-			LOG.log(Level.SEVERE, "Error " + e.getMessage(), e);
-		}
 	}
 }
--- src/main/java/com/jealcazars/jfxtail/control/LogFilesTabPane.java
+++ src/main/java/com/jealcazars/jfxtail/control/LogFilesTabPane.java
@@ -7,6 +7,10 @@ import java.util.logging.Logger;
 
 import com.jealcazars.jfxtail.utils.JfxTailAppPreferences;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 
 public class LogFilesTabPane extends TabPane {
@@ -14,6 +18,7 @@ public class LogFilesTabPane extends TabPane {
 
 	public LogFilesTabPane() {
 		List<String> files = JfxTailAppPreferences.getOpenFiles();
+
 		for (Iterator<String> iterator = files.iterator(); iterator.hasNext();) {
 			File file = new File(iterator.next());
 			if (file.exists()) {
@@ -22,19 +27,43 @@ public class LogFilesTabPane extends TabPane {
 				LOG.fine("File doesnt exists" + file.getAbsolutePath());
 			}
 		}
+
+		getSelectionModel().selectedItemProperty().addListener((event, oldValue, newValue) -> {
+			if (newValue != null) {
+				newValue.setGraphic(null);
+			}
+		});
 	}
 
 	public void addFile(File file, boolean setLastKnowFolder) {
-		LOG.fine("Adding file to panel: " + file.getAbsolutePath());
-		LogFileTab tabFile = new LogFileTab(file);
+		if (!isAlreadyOpen(file.getAbsolutePath())) {
+			LOG.fine("Adding file to panel: " + file.getAbsolutePath());
+			LogFileTab tabFile = new LogFileTab(file);
 
-		getTabs().add(tabFile);
+			getTabs().add(tabFile);
 
-		if (setLastKnowFolder) {
-			JfxTailAppPreferences.setLastKnowFolder(file.getParentFile().getAbsolutePath());
+			if (setLastKnowFolder) {
+				JfxTailAppPreferences.setLastKnowFolder(file.getParentFile().getAbsolutePath());
+			}
+
+			JfxTailAppPreferences.addToLastOpenFiles(file.getAbsolutePath());
+		} else {
+			LOG.fine("File already open");
+			Alert alert = new Alert(AlertType.NONE, "File is already open ", ButtonType.OK);
+			alert.setTitle("Warning");
+			alert.showAndWait();
 		}
+	}
 
-		JfxTailAppPreferences.addToLastOpenFiles(file.getAbsolutePath());
+	private boolean isAlreadyOpen(String absolutePath) {
+		Iterator<Tab> tabIt = getTabs().iterator();
+		while (tabIt.hasNext()) {
+			LogFileTab tab = (LogFileTab) tabIt.next();
+			if (tab.getFilePath().equals(absolutePath)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
